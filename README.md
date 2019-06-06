2dcd71aa054dc0d2329e123403cd8c92f37ad932
Aaron Miller <aaaronmiller@gmail.com>
2019-06-05 23:56:08 -0700

    midnight git push

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # unit-4-game
+The game is very close to being done but I didn't quite get there.  Currently the code breaks when trying to display the proper number of opponents after both the character and opposition have been chosen. Also once the player wins, the loop counter iterates to 3, and the actions which can then be taken also need to be coded (although they are essentialy the same as when loop=2, hence the reason I spent my time trying to make loop 2 complete, if loop 2 was still malfunctioning, loop 3 wouild as well)
+
+There also was some problems with defining unique id's to some of the actions, originally I had many of the actions operate based on the id of their parent div, however this caused some functions to iterate extra times as elements which had their parents id were triggered extra times. Close...
+
---
+++ assets/javascript/game.js
@@ -0,0 +1,263 @@
+// define variables
+phase = 1;
+myChar = "";
+badGuyChar = "";
+death = "no";
+myHp = "";
+badGuyHp = "";
+myPower = "";
+badGuyPower = "";
+characters = {cheta: {hp:100, power:20},pitbull: {hp:80, power:15},pitBull: {hp: 80, power:15},dalmation: {hp:120, power: 10},chihuahua: {hp:50, power: 5}};
+opponents = ["cheta", "pitbull", "dalmation", "chihuahua"];
+badGuyList = "";
+selectedBadGuy = "";
+killedBadGuy = "";
+//capture mouseclick select you
+// define functiopns
+function clearSelectMe () {
+    // document.getElementById("charSelect").innerHTML = " ";
+    $("#charSelect").html(" ");
+    console.log("cleared");
+};
+
+function clearSelectOpp () {
+    $("#badguys").html(" ");
+};
+
+function clearOpp () {
+    $("#opponent").html(" ");
+};
+
+function charSelector () {
+    myChar = event.srcElement.id;
+    myHp = eval('characters.' + myChar + ".hp");
+    myPower = eval('characters.' + myChar + ".power");
+    mySpot = opponents.indexOf(myChar);
+    opponents.splice(mySpot,1);
+    // break;   
+    // clearSelectMe ();
+};
+
+function badGuySelector () {
+    badGuyChar = event.srcElement.id;
+    badGuyHp = eval('characters.' + badGuyChar + ".hp");
+    badGuyPower = eval('characters.' + badGuyChar + ".power");
+    console.log(badGuyChar);
+    selectedBadGuy = opponents.indexOf(badGuyChar);
+    
+    opponents.splice(selectedBadGuy,1);
+    console.log (badGuyChar);
+    // console.log(selectedBadGuy);
+    console.log (opponents);
+    // break;   
+    // clearSelectMe ();
+};
+
+function youLoose () {
+    alert("YOU HAVE DIED!");
+    resetgame ();   
+}
+
+function fatality () {
+    alert("FATALITY! GOOD JOB! SELECT A NEW OPPONENT!");
+    phase = 1;
+    clearOpp ();
+    badGuyList = "";
+    killedBadGuy = opponents.indexOf(badGuyChar);
+    opponents.splice(killedBadGuy,1);
+    for (i=0; i < opponents.length; i++) {
+        badGuyList = badGuyList + '<img style:"text-align:center; float:left" id = "' + opponents[i] + '" src = "assets/images/' + opponents[i] + '.jpg">';
+        // badGuyList = badGuyList + '<h3 id =' +  + ' onClick="badGuySelector()">' + opponents[i] + '</h3>';
+        $("#badguys").html(badGuyList);
+        };
+
+}
+// alert(characters.cheta.hp);  
+//clear charSelect div
+// alert("booger");
+// $("#cheta").on("click", function() {
+$("#cheta").on("click", function() {
+    if(phase === 1) {
+        charName = document.getElementById("cheta");
+        clearSelectMe ();
+        charSelector(charName);
+        // document.getElementById("you").innerHTML = '<h3 id = ' + myChar + '>' + myChar + '<h3>';
+        // $("#you").html('<h3 id = ' + myChar + '>' + myChar + '<h3>');
+        $("#you").html('<img style:"text-align:center; float:left" id = "' + myChar + '" src = "assets/images/' + myChar + '.jpg"><br>');
+        // $("#badguys").html('<h3 id ="pitbull" onClick="badGuySelector()">Pitbull</h3><h3 id ="dalmation" onClick="badGuySelector()">dalmation</h3><h3 id="chihuahua" onClick="badGuySelector()">Chihuahua</h3>');
+        badGuyList = "";
+        //loop for displaying badguys
+        for (i=0; i <= opponents.length-1 ; i++) {
+            badGuyList = badGuyList + '<img style:"text-align:center; float:left" id = "' + opponents[i] + '" src = "assets/images/' + opponents[i] + '.jpg">';
+            console.log(badGuyList);
+            console.log(opponents);
+            console.log(i);
+            // badGuyList = badGuyList + '<h3 id =' +  + ' onClick="badGuySelector()">' + opponents[i] + '</h3>';
+            $("#badguys").html(badGuyList);
+            };
+        // myHp = characters.cheta.hp;
+        // myPower = characters.cheta.power;
+        $("#hp").text(myHp);
+        $("#power").text(myPower);
+        phase = 2;
+     
+    }
+        
+});
+$("#pitbull").on("click", function() {
+    if(phase === 1) {
+        charName = document.getElementById("pitbull");
+        clearSelectMe ();
+        charSelector(charName);
+        // document.getElementById("you").innerHTML = '<h3 id = ' + myChar + '>' + myChar + '<h3>';
+        // $("#you").html('<h3 id = ' + myChar + '>' + myChar + '<h3>');
+        $("#you").html('<img style:"text-align:center; float:left" id = "' + myChar + '" src = "assets/images/' + myChar + '.jpg"><br>');
+        // $("#badguys").html('<h3 id ="pitbull" onClick="badGuySelector()">Pitbull</h3><h3 id ="dalmation" onClick="badGuySelector()">dalmation</h3><h3 id="chihuahua" onClick="badGuySelector()">Chihuahua</h3>');
+        
+        //loop for displaying badguys
+        for (i=0; i< opponents.length; i++) {
+            badGuyList = badGuyList + '<img style:"text-align:center; float:left" id = "' + opponents[i] + '" src = "assets/images/' + opponents[i] + '.jpg">';
+            // badGuyList = badGuyList + '<h3 id =' +  + ' onClick="badGuySelector()">' + opponents[i] + '</h3>';
+            $("#badguys").html(badGuyList);
+            };
+        // myHp = characters.cheta.hp;
+        // myPower = characters.cheta.power;
+        $("#hp").text(myHp);
+        $("#power").text(myPower);
+        phase = 2;
+     
+    }
+        
+});
+$("#dalmation").on("click", function() {
+    if(phase === 1) {
+        charName = document.getElementById("dalmation");
+        clearSelectMe ();
+        charSelector(charName);
+        // document.getElementById("you").innerHTML = '<h3 id = ' + myChar + '>' + myChar + '<h3>';
+        // $("#you").html('<h3 id = ' + myChar + '>' + myChar + '<h3>');
+        $("#you").html('<img style:"text-align:center; float:left" id = "' + myChar + '" src = "assets/images/' + myChar + '.jpg"><br>');
+        // $("#badguys").html('<h3 id ="pitbull" onClick="badGuySelector()">Pitbull</h3><h3 id ="dalmation" onClick="badGuySelector()">dalmation</h3><h3 id="chihuahua" onClick="badGuySelector()">Chihuahua</h3>');
+        
+        //loop for displaying badguys
+        for (i=0; i< opponents.length; i++) {
+            badGuyList = badGuyList + '<img style:"text-align:center; float:left" id = "' + opponents[i] + '" src = "assets/images/' + opponents[i] + '.jpg">';
+            // badGuyList = badGuyList + '<h3 id =' +  + ' onClick="badGuySelector()">' + opponents[i] + '</h3>';
+            $("#badguys").html(badGuyList);
+            };
+        // myHp = characters.cheta.hp;
+        // myPower = characters.cheta.power;
+        $("#hp").text(myHp);
+        $("#power").text(myPower);
+        phase = 2;
+     
+    }
+        
+});
+$("#chihuahua").on("click", function() {
+    if(phase === 1) {
+        charName = document.getElementById("chihuahua");
+        clearSelectMe ();
+        charSelector(charName);
+        // document.getElementById("you").innerHTML = '<h3 id = ' + myChar + '>' + myChar + '<h3>';
+        // $("#you").html('<h3 id = ' + myChar + '>' + myChar + '<h3>');
+        $("#you").html('<img style:"text-align:center; float:left" id = "' + myChar + '" src = "assets/images/' + myChar + '.jpg"><br>');
+        // $("#badguys").html('<h3 id ="pitbull" onClick="badGuySelector()">Pitbull</h3><h3 id ="dalmation" onClick="badGuySelector()">dalmation</h3><h3 id="chihuahua" onClick="badGuySelector()">Chihuahua</h3>');
+        
+        //loop for displaying badguys
+        for (i=0; i< opponents.length; i++) {
+            badGuyList = badGuyList + '<img style:"text-align:center; float:left" id = "' + opponents[i] + '" src = "assets/images/' + opponents[i] + '.jpg">';
+            // badGuyList = badGuyList + '<h3 id =' +  + ' onClick="badGuySelector()">' + opponents[i] + '</h3>';
+            $("#badguys").html(badGuyList);
+            };
+        // myHp = characters.cheta.hp;
+        // myPower = characters.cheta.power;
+        $("#hp").text(myHp);
+        $("#power").text(myPower);
+        phase = 2;
+     
+    }
+        
+});
+
+$("#badguys").on("click", function() {
+    // if(phase === 1) { }
+// select a new opponent after winning
+    // for (i=0; i< opponents.length; i++) {
+    //     badGuyList = badGuyList + '<h3 id =' + opponents[i] + ' onClick="badGuySelector()">' + opponents[i] + '</h3>';
+    //     $("#badguys").html(badGuyList);
+    //     };
+    // };
+
+
+    
+    if(phase === 2) {
+    // clearSelectOpp ();
+    badGuySelector ();
+    // document.getElementById("opponent").innerHTML = '<h3 id = ' + badGuyChar + '>' + badGuyChar + '<h3>';
+    // $("#opponent").html('<h3 id = ' + badGuyChar + '>' + badGuyChar + '<h3>');
+    $("#opponent").html('<img style:"text-align:center; float:left" id = "' + badGuyChar + '" src = "assets/images/' + badGuyChar + '.jpg">');
+
+    //TODO: list remaining opponents here
+    // alert(opponents.length);
+    for (i=0; i < opponents.length; i++) {
+        // badGuyList = badGuyList + '<h3 id =' + opponents[i] + ' onClick="badGuySelector()">' + opponents[i] + '</h3>';
+        badGuyList = '<img style:"text-align:center; float:left" onClick="badGuySelector()" id = "' + opponents[i] + '" src = "assets/images/' + opponents[i] + '.jpg">';
+        $("#badguys").html(badGuyList);
+        };
+    phase = 3;
+    // alert("booger!");
+    // alert(opponents);
+    }
+// };
+});
+
+$("#you").click(function() {
+    clearSelectMe ();
+    // charSelect ("cheta" );
+    // alert("dog");
+});  
+
+$("#attacks").click(function() {
+    if (phase === 3) {
+        alert("POW!");
+        badGuyHp = badGuyHp - myPower;
+        myHp = myHp - badGuyPower;
+        myPower = myPower * 2;
+        $("#hp").text(myHp);
+        $("#power").text(myPower);
+        if (badGuyHp <= 0 && myHp > 0) {
+            fatality ();
+        } 
+        if (myHp <= 0) {
+            youLoose ();
+        }
+    }
+});  
+
+
+
+
+
+
+//put your character's image in "you" div
+
+//capture mouseclick select opp.
+
+//put your remaining opponents in the "badguys" div
+
+//put opponent in "opponent" div
+
+//start fight loop
+
+//capture mouseclick
+
+//apply damage
+
+//check for death
+
+//if death = yes do opponent select
+
+//increase attack power
+
+// capture another mouseclick
